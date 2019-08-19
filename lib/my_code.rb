afb542f7c685455d2ef26277db615dc21b3cddb2
Kayla Petersen <petersen1994@gmail.com>
2019-08-19 13:14:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,10 +58,9 @@ def reduce_to_any_true(source_array)
    value = false
   while i < source_array.length do
     if source_array[i] == true
-       value = true
-       return value
+      value = true
     else 
-      return value
+      value
     end  
     i += 1
   end
